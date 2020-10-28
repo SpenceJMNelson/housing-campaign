991f946afb57f423b54531cfa433bfa7be462e65
Spence Nelson <spenjmn@me.com>
2020-10-28 14:46:36 -0400

    update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ $( document ).ready(function() {
   $( ".question-card").click(function() {
     theCardId = $(this).attr('id');
     $( ".info-modal, body" ).toggleClass( "active" );
-    $("#info-modal-content").load(`/fragments/${theCardId}.html`);
+    $("#info-modal-content").load(`fragments/${theCardId}.html`);
   });
 });
