377bb6c2c37eada9abe1fd7f3bc2377c552d4507
Spence Nelson <spenjmn@me.com>
2020-10-29 08:42:08 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
   $( ".question-card").click(function() {
-    $(".info-modal").scrollTop());
     theCardId = $(this).attr('id');
     $( ".info-modal, body, .closelink-wrapper" ).toggleClass( "active" );
     $("#info-modal-content").load(`/housing-campaign/fragments/${theCardId}.html`);
@@ -11,5 +10,6 @@ $( document ).ready(function() {
 $( document ).ready(function() {
   $( ".closelink").click(function() {
     $( ".info-modal, body, .closelink-wrapper" ).toggleClass( "active" );
+    $(".info-modal").scrollTop());
   });
 });
