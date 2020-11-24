a8cebb8037ce4d03555d817dbca9853da24c2244
Spence Nelson <spenjmn@me.com>
2020-11-24 18:31:45 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
   $( ".question-card").click(function() {
     theCardId = $(this).attr('id');
     $( ".info-modal, body, .closelink-wrapper" ).toggleClass( "active" );
-    $("#info-modal-content").load(`https://spencejmnelson.github.io/housing-campaign/fragments/${theCardId}.html`);
+    $("#info-modal-content").load(`${location.origin}/fragments/${theCardId}.html`);
     $(".info-modal").scrollTop(0);
   });
 });
@@ -11,7 +11,7 @@ $( document ).ready(function() {
   $( "#sponsorship-link").click(function() {
     theCardId = $(this).attr('id');
     $( ".info-modal, body, .closelink-wrapper" ).toggleClass( "active" );
-    $("#info-modal-content").load(`https://spencejmnelson.github.io/housing-campaign/fragments/sponsorship.html`);
+    $("#info-modal-content").load(`${location.origin}/fragments/sponsorship.html`);
     $(".info-modal").scrollTop(0);
   });
 });
